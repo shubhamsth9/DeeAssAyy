19726b2d6c1e642c3af01389e0bf83cdabf8ec14
Shubham Singh Thakur <stfushubham@gmail.com>
2022-04-16 18:01:28 +0530

    Added solution - LeetHub

--- Children Sum Parent - GFG/children-sum-parent.cpp
+++ Children Sum Parent - GFG/children-sum-parent.cpp
@@ -86,52 +86,21 @@ Node* buildTree(string str) {
 
 
  // } Driver Code Ends
-/*Complete the function below
 
-struct Node
-{
-    int data;
-    struct Node* left;
-    struct Node* right;
-    
-    Node(int x){
-        data = x;
-        left = right = NULL;
-    }
-};
-*/
 
 class Solution{
     public:
     
-    int isValid(Node* root){
-        if(root->left == NULL && root->right == NULL) return 1;
-        if(root->left != NULL && root->right != NULL){
-            if(root->left->data + root->right->data == root->data)
-                return 1;
-            return 0;
-        }
-        if(root->left == NULL && root->right != NULL){
-            if(root->right->data == root->data)
-                return 1;
-            return 0;
-        }
-        if(root->left != NULL && root->right == NULL){
-            if(root->left->data == root->data)
-                return 1;
-            return 0;
-        }
-    }
-    
     int isSumProperty(Node *root)
     {
         if(root == NULL) return 1;
+        if(root->left == NULL && root->right == NULL) return 1;
         
-        if(isValid(root))
-            return (isSumProperty(root->left) & isSumProperty(root->right));
+        int sum = 0;
+        if(root->left != NULL) sum += root->left->data;
+        if(root->right != NULL) sum += root->right->data;
         
-        else
-            return 0;
+        return ((sum == root->data) && (isSumProperty(root->left)) && isSumProperty(root->right));
     }
 };
 
