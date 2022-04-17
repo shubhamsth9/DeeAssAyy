28bea20a910fe5a58350908fa9b547d2361f78dc
Shubham Singh Thakur <stfushubham@gmail.com>
2022-04-17 16:30:20 +0530

    Added solution - LeetHub

--- Check for Balanced Tree - GFG/check-for-balanced-tree.cpp
+++ Check for Balanced Tree - GFG/check-for-balanced-tree.cpp
@@ -109,10 +109,8 @@ class Solution{
         int lh = height(root->left);
         int rh = height(root->right);
         
-        if(lh == -1 || rh == -1) return -1;
-        if(abs(lh - rh) <= 1) return max(lh, rh)+1;
-        return -1;
-        
+        if(lh == -1 || rh == -1 || abs(lh - rh) > 1) return -1;
+        return max(lh, rh)+1;
     }
     
     bool isBalanced(Node *root)
