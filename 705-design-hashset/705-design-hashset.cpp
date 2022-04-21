f8e26b62b5ed5a56d7203a736658426133b4f3cb
Shubham Singh Thakur <stfushubham@gmail.com>
2022-04-21 12:58:11 +0530

    Time: 206 ms (30.27%), Space: 180 MB (15.87%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,22 +1,53 @@
 class MyHashSet {
 private:
+    const static int n = 1000001;
+    vector<int> myHash;
 public:
-    int myHash[1000001] = {0};
-    
     MyHashSet() {
-        
+        myHash.resize(1000001, -1);
     }
     
     void add(int key) {
-        myHash[key] = 1;
+        if(myHash[key%n] == -1 || myHash[key%n] == key){
+            myHash[key%n] = key;
+        }
+        else{
+            int i = key;
+            while(myHash[i%n] != -1){
+                if(myHash[i%n] == key) break;
+                i++;
+            }
+            myHash[i%n] = key;
+        }
     }
     
     void remove(int key) {
-        myHash[key] = 0;
+        if(myHash[key%n] == -1 || myHash[key%n] == key){
+            myHash[key%n] = -1;
+        }
+        else{
+            int i = key;
+            while(myHash[i%n] != key){
+                if(myHash[i%n] == -1) break;
+                i++;
+            }
+            myHash[i%n] = -1;
+        }
+        
     }
     
     bool contains(int key) {
-        return myHash[key];
+        if(myHash[key%n] == -1)return false;
+        if(myHash[key%n] == key) return true;
+        else{
+            int i = key;
+            while(myHash[i%n] != key){
+                if(myHash[i%n] == -1) return false;
+                i++;
+            }
+            if(myHash[i%n] == key) return true;
+        }
+        return false;
     }
 };
 
