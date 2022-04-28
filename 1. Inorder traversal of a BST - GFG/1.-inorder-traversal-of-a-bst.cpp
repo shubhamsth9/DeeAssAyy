1b51f05c4be9f77182ea7eccf05ca8a9020edf60
Shubham Singh Thakur <stfushubham@gmail.com>
2022-04-28 20:21:45 +0530

    Added solution - LeetHub

---
+++ 1. Inorder traversal of a BST - GFG/1.-inorder-traversal-of-a-bst.cpp
@@ -0,0 +1,73 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+};
+
+Node *createNewNode(int value) {
+    Node *temp = new Node;
+    temp->data = value;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+Node *newNode(Node *root, int data) {
+    if (root == NULL)
+        root = createNewNode(data);
+    else if (data < root->data)
+        root->left = newNode(root->left, data);
+    else
+        root->right = newNode(root->right, data);
+
+    return root;
+}
+
+
+ // } Driver Code Ends
+// User function Template for C++
+
+// Function to return a list containing the inorder traversal of the BST.
+void util(Node* root, vector<int>& res){
+    if(root == NULL) return;
+    util(root->left, res);
+    res.push_back(root->data);
+    util(root->right, res);
+}
+
+vector<int> inOrder(Node *root) {
+    vector<int> res;
+    if(root == NULL) return res;
+    util(root, res);
+    return res;
+}
+
+// { Driver Code Starts.
+
+int main() {
+
+    int testcases;
+    cin >> testcases;
+    while (testcases--) {
+        Node *root = NULL;
+        int sizeOfArray;
+        cin >> sizeOfArray;
+        int arr[sizeOfArray];
+
+        for (int i = 0; i < sizeOfArray; i++) cin >> arr[i];
+
+        for (int i = 0; i < sizeOfArray; i++) {
+            root = newNode(root, arr[i]);
+        }
+
+        vector<int> res = inOrder(root);
+        for (int i = 0; i < res.size(); i++) cout << res[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
