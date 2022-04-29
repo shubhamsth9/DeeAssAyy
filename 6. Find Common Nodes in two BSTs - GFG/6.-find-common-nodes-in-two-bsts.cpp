2e109e3e2f290211ccc17557db6ff301f847e2be
Shubham Singh Thakur <stfushubham@gmail.com>
2022-04-29 08:39:36 +0530

    Added solution - LeetHub

--- 6. Find Common Nodes in two BSTs - GFG/6.-find-common-nodes-in-two-bsts.cpp
+++ 6. Find Common Nodes in two BSTs - GFG/6.-find-common-nodes-in-two-bsts.cpp
@@ -86,27 +86,73 @@ class Solution
 {
     public:
     //Function to find the nodes that are common in both BST.
-    bool find(Node* root, int x){
-        if(root == NULL) return false;
-        if(root->data > x) return find(root->left, x);
-        else if(root->data < x) return find(root->right, x);
-        return true;
-    }
-    
-    void inorder(Node* root, Node* temp, vector<int>& res){
-        if(root == NULL) return;
-        inorder(root->left, temp, res);
-        
-        if(find(temp, root->data)) res.push_back(root->data);
-        
-        inorder(root->right, temp, res);
-    }
     
     vector <int> findCommon(Node *root1, Node *root2)
     {
         vector<int> res;
         if(root1 == NULL || root2== NULL) return res;
-        inorder(root1, root2, res);
+        // Create two stacks for two inorder traversals
+    stack<Node *> stack1, s1, s2;
+  
+    while (1)
+    {
+        // push the Nodes of first tree in stack s1
+        if (root1)
+        {
+            s1.push(root1);
+            root1 = root1->left;
+        }
+  
+        // push the Nodes of second tree in stack s2
+        else if (root2)
+        {
+            s2.push(root2);
+            root2 = root2->left;
+        }
+  
+        // Both root1 and root2 are NULL here
+        else if (!s1.empty() && !s2.empty())
+        {
+            root1 = s1.top();
+            root2 = s2.top();
+  
+            // If current keys in two trees are same
+            if (root1->data == root2->data)
+            {
+                res.push_back(root1->data);
+                s1.pop();
+                s2.pop();
+  
+                // move to the inorder successor
+                root1 = root1->right;
+                root2 = root2->right;
+            }
+  
+            else if (root1->data < root2->data)
+            {
+                // If Node of first tree is smaller, than that of
+                // second tree, then its obvious that the inorder
+                // successors of current Node can have same value
+                // as that of the second tree Node. Thus, we pop
+                // from s2
+                s1.pop();
+                root1 = root1->right;
+  
+                // root2 is set to NULL, because we need
+                // new Nodes of tree 1
+                root2 = NULL;
+            }
+            else if (root1->data > root2->data)
+            {
+                s2.pop();
+                root2 = root2->right;
+                root1 = NULL;
+            }
+        }
+  
+        // Both roots and both stacks are empty
+        else  break;
+    }
         
         return res;
     }
