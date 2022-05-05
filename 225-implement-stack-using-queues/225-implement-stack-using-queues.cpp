9c6a95c8ebcbeab033dc704ced238516e40ef918
Shubham Singh Thakur <stfushubham@gmail.com>
2022-05-06 00:06:44 +0530

    Time: 0 ms (100.00%), Space: 7 MB (19.21%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -1,37 +1,34 @@
 class MyStack {
 public:
-    queue<int> q;
-    int peek = -1;
+    queue<int> q1, q2;
     MyStack() {
         
     }
     
     void push(int x) {
-        peek = x;
-        q.push(x);
+        while(!q1.empty()){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        q1.push(x);
+        while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
+        }
     }
     
     int pop() {
-        int n = q.size();
-       n-=1; 
-       while(n--){
-          peek = q.front();     // maintining the top element 
-           q.push(peek);
-           q.pop();
-       } 
-        
-       int ret = q.front();     // storing the element to return 
-        q.pop();                // removing the element permanently
-        
-        return ret;
+        int k = q1.front();
+        q1.pop();
+        return k;
     }
     
     int top() {
-        return peek ;
+        return q1.front();
     }
     
     bool empty() {
-        return q.size()==0;
+        return q1.size()==0;
     }
 };
 
