60deb618ce4e868c611b32975f21ac1d71ce8b8f
Shubham Singh Thakur <stfushubham@gmail.com>
2022-05-08 14:56:43 +0530

    Added solution - LeetHub

--- 7. K Most occurring elements - GFG/7.-k-most-occurring-elements.cpp
+++ 7. K Most occurring elements - GFG/7.-k-most-occurring-elements.cpp
@@ -17,13 +17,12 @@ class Solution
     	for(int i=0; i<n; i++){
     	    umap[arr[i]]++;
     	}
-    	priority_queue<int, vector<int>, greater<int>> pq;
+    	priority_queue<int> pq;
     	for(auto it: umap){
     	    pq.push(it.second);
-    	    if(pq.size() > k) pq.pop();
     	}
     	int sum = 0;
-    	while(pq.size()){
+    	while(k--){
     	    sum += pq.top();
     	    pq.pop();
     	}
