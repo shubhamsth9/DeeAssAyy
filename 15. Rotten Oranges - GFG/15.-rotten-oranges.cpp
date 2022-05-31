6a58b06663f9923d840f36ee77ea54d9bc1ace00
Shubham Singh Thakur <stfushubham@gmail.com>
2022-05-31 10:47:42 +0530

    Added solution - LeetHub

--- 15. Rotten Oranges - GFG/15.-rotten-oranges.cpp
+++ 15. Rotten Oranges - GFG/15.-rotten-oranges.cpp
@@ -8,20 +8,20 @@ class Solution
     public:
     //Function to find minimum time required to rot all oranges.
     int orangesRotting(vector<vector<int>>& grid) {
-        int res=0;
+        int res=0, total_oranges = 0, cnt = 0;
         queue<pair<int, int>> q;
         
         for(int i=0; i<grid.size(); i++){
             for(int j=0; j<grid[0].size(); j++){
-                if(grid[i][j] == 2){
-                    q.push({i, j});
-                }
+                if(grid[i][j] == 2) q.push({i, j});
+                if(grid[i][j] != 0) total_oranges++;
             }
         }
         
         while(!q.empty()){
             int flag = 0;
             int n = q.size();
+            cnt += n;
             
             for(int it=0; it<n; it++){
                 auto tmp = q.front(); q.pop();
@@ -52,15 +52,9 @@ class Solution
             else break;
         }
         
-        for(int i=0; i<grid.size(); i++){
-            for(int j=0; j<grid[0].size(); j++){
-                if(grid[i][j] == 1){
-                    res = -1;
-                }
-            }
-        }
+        if(total_oranges == cnt) return res;
         
-        return res;
+        return -1;
     }
 };
 
