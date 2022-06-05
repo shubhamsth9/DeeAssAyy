66866bac7ae582b16212bddba4621c6c546386ce
Shubham Singh Thakur <stfushubham@gmail.com>
2022-06-05 23:24:24 +0530

    Added solution - LeetHub

--- 19. Minimum Spanning Tree - GFG/19.-minimum-spanning-tree.cpp
+++ 19. Minimum Spanning Tree - GFG/19.-minimum-spanning-tree.cpp
@@ -14,42 +14,30 @@ class Solution
     {
        vector<int> key(V, INT_MAX);
        vector<bool> mst(V, false);
-       //vector<int> parent(V, -1);
        
        key[0] = 0;
-       //parent[0] = -1;
+       int sum = 0;
        
        for(int i = 0 ; i < V ; i++){
            int mini = INT_MAX;
            int u;
-           //Step 1 : Find node with min weight using key vector
-           //We can also use priority queue to reduce the time complexity
            for(int v = 0 ; v < V ; v++){
                if(mst[v] == false && key[v] < mini){
-                   //node with idx v has min weight
                    u = v;
                    mini = key[v];
                }
            }
-           //Step 2 : Mark min weight node true
            mst[u] = true;
-           
-           //Step 3 : Traverse adjacent of u 
+           sum += key[u];
            for(auto neighbour : adj[u]){
-               int v = neighbour[0]; //Neighbour node of u
-               int w = neighbour[1]; //Weight from u to v
-               //3.1 : If v is not in MST and its weight is < previous weight then update it
+               int v = neighbour[0];
+               int w = neighbour[1];
                if(mst[v] == false && w < key[v]){
                    key[v] = w;
-                   //Initialize parent of v to u
-                   //parent[v] = u;
                }
            }
        }
-       int sum = 0;
-       for(auto it : key){
-           sum += it;
-       }
+      
        return sum;
         // int res = 0;
         // vector<int> key(V, INT_MAX);
