4fbccf19feff797874bb6cd364313aaf62d6848d
Shubham Singh Thakur <stfushubham@gmail.com>
2022-06-17 18:26:47 +0530

    Added solution - LeetHub

---
+++ 1. Fibonacci Numbers - Bottom Up DP - GFG/1.-fibonacci-numbers-bottom-up-dp.cpp
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+#include <fstream>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+// You need to complete this function
+
+
+class Solution
+{
+    public:
+    //Function to find the nth fibonacci number using bottom-up approach.
+    long long findNthFibonacci(int n)
+    {
+        long long dp[n+1] = {1, 1};
+        for(int i=2; i<n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n-1];
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+ {
+    //taking testcases
+    int testcases;
+    cin>>testcases;
+    
+    while(testcases--)
+    {
+        //taking nth fibonacci 
+        //to be found
+        int number;
+        cin>>number;
+        
+        //calling function findNthFibonacci()
+        //and passing number as parameter
+        Solution obj;
+        cout<<obj.findNthFibonacci(number)<<endl;
+    }
+    
+	return 0;
+}
+  // } Driver Code Ends
