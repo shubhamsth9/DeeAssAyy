a0d9ec80280025acdc5433dcfb7a7d89732ea60e
Shubham Singh Thakur <stfushubham@gmail.com>
2022-06-25 12:18:16 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (56.46%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -13,12 +13,16 @@ public:
 //     }
     
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 1));
+        //vector<vector<int>> dp(m, vector<int>(n, 1));
+        vector<int> prev(n, 1);
         
-        for(int i=1; i<m; i++)
-            for(int j=1; j<n; j++)
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-        
-        return dp[m-1][n-1];
+        for(int i=1; i<m; i++){
+            vector<int> curr(n, 1);
+            for(int j=1; j<n; j++){
+                curr[j] = prev[j] + curr[j-1];
+            }
+            prev = curr;
+        }
+        return prev[n-1];
     }
 };
