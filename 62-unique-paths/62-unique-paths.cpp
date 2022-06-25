a29e3f6fdcb136c763a79dabf958b80564a93781
Shubham Singh Thakur <stfushubham@gmail.com>
2022-06-25 11:23:35 +0530

    Time: 5 ms (17.65%), Space: 6.5 MB (43.86%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -6,8 +6,10 @@ public:
         if(m < 0 || n < 0) return 0;
         if(dp[m][n] != -1) return dp[m][n];
         
-        dp[m][n] = f(m-1, n, dp) + f(m, n-1, dp);
-        return dp[m][n];
+        dp[m-1][n] = f(m-1, n, dp);
+        dp[m][n-1] = f(m, n-1, dp);
+        
+        return dp[m][n] = dp[m-1][n] + dp[m][n-1];
     }
     
     int uniquePaths(int m, int n) {
