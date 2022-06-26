7c21357033b95e2211c78b3079ff329bdd6345f4
Shubham Singh Thakur <stfushubham@gmail.com>
2022-06-26 08:46:56 +0530

    Time: 2 ms (48.64%), Space: 5.8 MB (90.15%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -14,15 +14,22 @@ public:
     
     int uniquePaths(int m, int n) {
         //vector<vector<int>> dp(m, vector<int>(n, 1));
-        vector<int> prev(n, 1);
+//         vector<int> prev(n, 1);
         
-        for(int i=1; i<m; i++){
-            vector<int> curr(n, 1);
-            for(int j=1; j<n; j++){
-                curr[j] = prev[j] + curr[j-1];
-            }
-            prev = curr;
+//         for(int i=1; i<m; i++){
+//             vector<int> curr(n, 1);
+//             for(int j=1; j<n; j++){
+//                 curr[j] = prev[j] + curr[j-1];
+//             }
+//             prev = curr;
+//         }
+//         return prev[n-1];
+        int N = m+n-2, R = min(m-1, n-1);
+        cout<<"N = "<<N<<" R = "<<R<<endl;
+        double res = 1;
+        for(int i=1; i<=R; i++){
+            res = res * (N-R+i)/i;
         }
-        return prev[n-1];
+        return (int)res;
     }
 };
