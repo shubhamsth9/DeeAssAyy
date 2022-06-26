5e034ab13f0d415f97a8492c05b1a5ab55a671f8
Shubham Singh Thakur <stfushubham@gmail.com>
2022-06-26 11:40:53 +0530

    Time: 8 ms (89.46%), Space: 10.3 MB (5.94%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int f(int m, int n, vector<vector<int>>& grid, vector<vector<int>>& dp){
-        //if(m < 0 || n < 0) return INT_MAX;
         
         if(m == 0 && n == 0) return grid[0][0];
         
         if(dp[m][n] != -1) return dp[m][n];
+        
         int up = INT_MAX, left = INT_MAX;
+        
         if(m-1 >= 0) up = grid[m][n] + f(m-1, n, grid, dp);
         if(n-1 >= 0) left = grid[m][n] + f(m, n-1, grid, dp);
+        
         return dp[m][n] = min(up, left);
     }
     
     int minPathSum(vector<vector<int>>& grid) {
+        
         int m=grid.size(), n=grid[0].size();
         vector<vector<int>> dp(m, vector<int>(n, -1));
         
