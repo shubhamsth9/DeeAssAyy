9e947fb0e1cce4150762abbbf73cebf51378f254
Shubham Singh Thakur <stfushubham@gmail.com>
2022-08-05 11:47:20 +0530

    Time: 103 ms (86.51%), Space: 41.4 MB (78.29%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
+        int n = nums.size();
+        
         int cnt = 0, sum = 0;
+        
         unordered_map<int, int> umap;
-        for(int i=0; i<nums.size(); i++){
+        
+        for(int i=0; i<n; i++){
             sum += nums[i];
             if(sum == k) cnt++;
-            if(umap.count(sum-k)) cnt += umap[sum-k];
+            if(umap.find(sum-k) != umap.end()) cnt += umap[sum-k];
             umap[sum]++;
         }
+        
         return cnt;
     }
 };
