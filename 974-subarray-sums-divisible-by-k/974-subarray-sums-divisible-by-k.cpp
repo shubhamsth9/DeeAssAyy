071016b0e1850bba568520a195ede5ad6f0a94ae
Shubham Singh Thakur <stfushubham@gmail.com>
2022-08-06 23:19:50 +0530

    Time: 76 ms (70.04%), Space: 30.2 MB (87.98%) - LeetHub

--- 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        int cnt = 0, n=nums.size(), sum=0;
-        unordered_map<int, int> umap;
-        umap[0]++;
+        vector<int> freq(k, 0);
+        freq[0] = 1;
+        int cnt = 0, n = nums.size(), sum=0;
         
         for(int i=0; i<n; i++){
             sum += nums[i];
-            
             int rem = sum%k;
             if(rem < 0) rem += k;
-            //if(rem == 0) cnt++;
-            if(umap.find(rem) != umap.end()){
-                cnt += umap[rem];
-            }
-            umap[rem]++;
+            cnt += freq[rem];
+            freq[rem]++;
         }
-        
         return cnt;
+        
+//         int cnt = 0, n=nums.size(), sum=0;
+//         unordered_map<int, int> umap;
+//         umap[0]++;
+        
+//         for(int i=0; i<n; i++){
+//             sum += nums[i];
+            
+//             int rem = sum%k;
+//             if(rem < 0) rem += k;
+//             //if(rem == 0) cnt++;
+//             if(umap.find(rem) != umap.end()){
+//                 cnt += umap[rem];
+//             }
+//             umap[rem]++;
+//         }
+        
+//         return cnt;
     }
 };
