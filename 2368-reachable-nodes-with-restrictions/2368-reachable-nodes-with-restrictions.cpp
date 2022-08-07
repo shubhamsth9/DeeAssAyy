f5250ba30f12ba669393c2b861dd628aed573197
Shubham Singh Thakur <stfushubham@gmail.com>
2022-08-07 17:47:29 +0530

    Time: 1014 ms (11.11%), Space: 166.1 MB (11.11%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
@@ -1,25 +1,8 @@
 class Solution {
 public:
-//     void dfs(int i, int n, vector<bool>& vis, vector<vector<int>>& graph){
-//         vis[i] = true;
-        
-//         if(graph[i][0] == 1){
-//             graph[0][i] = graph[i][0] = 0;
-//         }
-        
-//         for(int j=i; j<n; j++){
-//             if(graph[i][j] == 1){
-//                 graph[i][j] == 0;
-//                 if(!vis[j]){
-//                     dfs(j, n, vis, graph);
-//                 }
-//             }
-//         }
-//     }
-    
     int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& rest) {
         vector<vector<int>> graph(n);
-        vector<bool> vis(n, false);
+        //vector<bool> vis(n, false);
         unordered_set<int> uset(rest.begin(), rest.end());
         
         for(int i=0; i<n-1; i++){
@@ -29,7 +12,7 @@ public:
         }
         
         
-        vis[0] = true;
+        //vis[0] = true;
         queue<int> q;
         q.push(0);
         int res = 0;
@@ -38,10 +21,11 @@ public:
             int size = q.size();
             while(size--){
                 int temp = q.front(); q.pop();
-                vis[temp] = true;
+                //vis[temp] = true;
                 res++;
+                uset.insert(temp);
                 for(auto it:graph[temp]){
-                    if(!vis[it] && uset.find(it) == uset.end()){
+                    if(uset.find(it) == uset.end()){
                         q.push(it);
                     }
                 }
