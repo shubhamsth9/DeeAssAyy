5b9c5ee42efe95f24cd6be3621deedfb5e767de8
Shubham Singh Thakur <stfushubham@gmail.com>
2022-08-17 09:58:34 +0530

    Time: 111 ms (53.65%), Space: 48.4 MB (45.07%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
-    bool util(int i, vector<int>& memo, vector<int>& nums){
-        if(i == nums.size()-1 ) return true;
+//     bool util(int i, vector<int>& memo, vector<int>& nums){
+//         if(i == nums.size()-1 ) return true;
         
-        if(memo[i] != -1) return memo[i];
+//         if(memo[i] != -1) return memo[i];
         
-        for(int k = 1; k <= nums[i]; k++){
-            if(util(i+k, memo, nums))
-                return memo[i] = true;
-        }
-        return memo[i] = false;
-    }
+//         for(int k = 1; k <= nums[i]; k++){
+//             if(util(i+k, memo, nums))
+//                 return memo[i] = true;
+//         }
+//         return memo[i] = false;
+//     }
+    
     bool canJump(vector<int>& nums) {
-        vector<int> memo(nums.size(), -1);
-        return util(0, memo, nums);
+        
+        int reach = 0;
+        
+        for(int i=0; i<nums.size(); i++){
+            if(i > reach) return false;
+            reach = max(reach, i+nums[i]);
+            if(reach >= nums.size()-1) return true;
+        }
+        
+        return true;
+        // vector<int> memo(nums.size(), -1);
+        // return util(0, memo, nums);
     }
 };
