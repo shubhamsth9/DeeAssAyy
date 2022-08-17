b400a8a89224addd589307b7068db2d75e5fc955
Shubham Singh Thakur <stfushubham@gmail.com>
2022-08-17 11:00:58 +0530

    Time: 3 ms (76.26%), Space: 9.1 MB (72.88%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -6,8 +6,15 @@ public:
         
         int i=m-1, j=n-1, k=n+m-1;
         while(i>=0 && j>=0){
-            if(a[i] > b[j]) a[k--] = a[i--];
-            else a[k--] = b[j--];
+            if(a[i] > b[j]){
+                a[k] = a[i];
+                i--;
+            }
+            else{
+                a[k] = b[j];
+                j--;
+            }
+            k--;
         }
         while(i>=0) a[k--] = a[i--];
         while(j>=0) a[k--] = b[j--];
