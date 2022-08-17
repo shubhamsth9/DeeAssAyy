4158ae4d60e6002952efd8248a1fbf795e9139ee
Shubham Singh Thakur <stfushubham@gmail.com>
2022-08-17 10:59:52 +0530

    Time: 4 ms (54.85%), Space: 9.2 MB (30.17%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     void merge(vector<int>& a, int m, vector<int>& b, int n) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
         int i=m-1, j=n-1, k=n+m-1;
         while(i>=0 && j>=0){
             if(a[i] > b[j]) a[k--] = a[i--];
