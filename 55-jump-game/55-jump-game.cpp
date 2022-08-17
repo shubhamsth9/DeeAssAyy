ed5b11235c9cf5af504012e5b69bca09c744f671
Shubham Singh Thakur <stfushubham@gmail.com>
2022-08-17 10:00:18 +0530

    Time: 68 ms (94.26%), Space: 48.2 MB (93.51%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,18 +1,8 @@
 class Solution {
 public:
-//     bool util(int i, vector<int>& memo, vector<int>& nums){
-//         if(i == nums.size()-1 ) return true;
-        
-//         if(memo[i] != -1) return memo[i];
-        
-//         for(int k = 1; k <= nums[i]; k++){
-//             if(util(i+k, memo, nums))
-//                 return memo[i] = true;
-//         }
-//         return memo[i] = false;
-//     }
-    
     bool canJump(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         
         int reach = 0, n = nums.size();
         
@@ -23,7 +13,5 @@ public:
         }
         
         return true;
-        // vector<int> memo(nums.size(), -1);
-        // return util(0, memo, nums);
     }
 };
