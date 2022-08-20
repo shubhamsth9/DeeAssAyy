1acbba26070b6e73375ca36154b97e1bb2261599
Shubham Singh Thakur <stfushubham@gmail.com>
2022-08-20 12:42:34 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (100.00%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
-    void gameOfLife(vector<vector<int>>& board) {
-        int n = board.size(), m = board[0].size();
+    int check(int i, int j, int m, int n, vector<vector<int>>& board){
+        int cnt = 0;
         
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                int count=0;
-                for(int a=max(i-1, 0); a<min(i+2,n); a++){
-                    for(int b=max(j-1, 0); b<min(j+2, m); b++){
-                        if(board[a][b] & 1) count++;
-                    }
+        for(int I=max(0, i-1); I<min(m, i+2); I++){
+            for(int J=max(0, j-1); J<min(n, j+2); J++){
+                if(I == i && J == j) continue;
+                if(board[I][J] == 1 || board[I][J] == 3) cnt++;
+            }
+        }
+        return cnt;
+    }
+    
+    void gameOfLife(vector<vector<int>>& board) {
+        int m = board.size(), n = board[0].size();
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                int cnt = check(i, j, m, n, board);
+                if(board[i][j] == 0 && cnt == 3){
+                    board[i][j] = 2;
                 }
-                if(count == 3 || count-board[i][j] == 3)
-                    board[i][j] |= 2;
+                else if(board[i][j] == 1){
+                    if(cnt < 2 || cnt > 3) board[i][j] = 3;
+                }
                 
             }
         }
         
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                board[i][j] >>= 1;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(board[i][j] == 2) board[i][j] = 1;
+                else if(board[i][j] == 3) board[i][j] = 0;
             }
         }
     }
