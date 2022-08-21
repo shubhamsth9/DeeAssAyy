891cd790da09b97bb914f40b41f9e51b23b8e441
Shubham Singh Thakur <stfushubham@gmail.com>
2022-08-21 23:14:27 +0530

    Time: 91 ms (37.63%), Space: 24.1 MB (18.37%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPal(int i, int j, string s){
+        while(i<j){
+            if(s[i] != s[j]) return false;
+            i++, j--;
+        }
+        return true;
+    }
+    
+    bool validPalindrome(string s) {
+        int i=0, j=s.size()-1;
+        while(i<j){
+            if(s[i] != s[j]){
+                return isPal(i+1, j, s) || isPal(i, j-1, s);
+            }
+            i++, j--;
+        }
+        return true;
+    }
+};
