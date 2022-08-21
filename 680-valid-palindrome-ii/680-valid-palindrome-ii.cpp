cff10ea8d3f3af30e6b1079d5a2a43ad853dc4c7
Shubham Singh Thakur <stfushubham@gmail.com>
2022-08-21 23:15:15 +0530

    Time: 43 ms (98.80%), Space: 24.2 MB (16.62%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -9,6 +9,9 @@ public:
     }
     
     bool validPalindrome(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
         int i=0, j=s.size()-1;
         while(i<j){
             if(s[i] != s[j]){
