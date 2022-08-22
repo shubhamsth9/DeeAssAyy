01c0a42eba196d14d1a5ddcba80460b330a55fc7
Shubham Singh Thakur <stfushubham@gmail.com>
2022-08-23 00:21:23 +0530

    Time: 338 ms (83.42%), Space: 92.3 MB (68.69%) - LeetHub

---
+++ 1499-max-value-of-equation/1499-max-value-of-equation.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMaxValueOfEquation(vector<vector<int>>& pts, int k) {
+        int res = INT_MIN, n = pts.size();
+        priority_queue<pair<int, int>> pq;
+        
+        pq.push({pts[0][1] - pts[0][0], pts[0][0]});
+        
+        for(int i=1; i<n; i++){
+            int temp = pts[i][0] + pts[i][1];
+            while(!pq.empty() && pts[i][0] - pq.top().second > k) pq.pop();
+            if(!pq.empty()) res = max(res, temp+pq.top().first);
+            pq.push({pts[i][1]-pts[i][0], pts[i][0]});
+        }
+        return res;
+    }
+};
