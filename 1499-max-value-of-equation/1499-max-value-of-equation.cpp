c1f8f6ee1a0ab235c537eb099e3d92d9ef829178
Shubham Singh Thakur <stfushubham@gmail.com>
2022-08-23 00:23:33 +0530

    Time: 573 ms (26.16%), Space: 92.3 MB (68.69%) - LeetHub

--- 1499-max-value-of-equation/1499-max-value-of-equation.cpp
+++ 1499-max-value-of-equation/1499-max-value-of-equation.cpp
@@ -4,15 +4,16 @@ public:
         // ios_base::sync_with_stdio(false);
         // cin.tie(NULL);
         
-        int res = INT_MIN, n = pts.size();
+        int res = INT_MIN;
         priority_queue<pair<int, int>> pq;
         
         pq.push({pts[0][1] - pts[0][0], pts[0][0]});
         
-        for(int i=1; i<n; i++){
-            int temp = pts[i][0] + pts[i][1];
+        for(int i=1; i<pts.size(); i++){
+            
             while(!pq.empty() && pts[i][0] - pq.top().second > k) pq.pop();
-            if(!pq.empty()) res = max(res, temp+pq.top().first);
+            if(!pq.empty()) res = max(res, pts[i][0] + pts[i][1] + pq.top().first);
+            
             pq.push({pts[i][1]-pts[i][0], pts[i][0]});
         }
         return res;
