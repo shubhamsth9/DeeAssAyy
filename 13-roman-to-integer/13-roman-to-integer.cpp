a7ad0cb9636aafac30529415d407c2eb7b850520
Shubham Singh Thakur <stfushubham@gmail.com>
2022-08-23 23:07:41 +0530

    Time: 34 ms (17.44%), Space: 7.8 MB (44.23%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -16,9 +16,7 @@ public:
         int res = umap[s.back()], n = s.size();
         
         for(int i=n-2; i>=0; i--){
-            if(s[i] == 'I' && (s[i+1] == 'V' || s[i+1] == 'X')) res -= 1;
-            else if(s[i] == 'X' && (s[i+1] == 'L' || s[i+1] == 'C')) res -= 10;
-            else if(s[i] == 'C' && (s[i+1] == 'D' || s[i+1] == 'M')) res -= 100;
+            if(umap[s[i]] < umap[s[i+1]]) res -= umap[s[i]];
             else res += umap[s[i]];
         }
         return res;
