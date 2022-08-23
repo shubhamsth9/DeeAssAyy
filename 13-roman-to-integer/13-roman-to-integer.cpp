1c627ae65644f9b76c4ca1a41e79378bebd710a0
Shubham Singh Thakur <stfushubham@gmail.com>
2022-08-23 23:04:51 +0530

    Time: 24 ms (36.57%), Space: 7.8 MB (50.94%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -3,17 +3,17 @@ public:
     int romanToInt(string s) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        unordered_map<char, int> umap;
-        umap['I'] = 1;
-        umap['V'] = 5;
-        umap['X'] = 10;
-        umap['L'] = 50;
-        umap['C'] = 100;
-        umap['D'] = 500;
-        umap['M'] = 1000;
-        int res = 0, n = s.size();
+        unordered_map<char, int> umap = {
+            { 'I' , 1 },
+           { 'V' , 5 },
+           { 'X' , 10 },
+           { 'L' , 50 },
+           { 'C' , 100 },
+           { 'D' , 500 },
+           { 'M' , 1000 }
+        };
         
-        res += umap[s.back()];
+        int res = umap[s.back()], n = s.size();
         
         for(int i=n-2; i>=0; i--){
             if(s[i] == 'I' && (s[i+1] == 'V' || s[i+1] == 'X')) res -= 1;
