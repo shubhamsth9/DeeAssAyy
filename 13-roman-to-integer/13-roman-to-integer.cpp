772853f3a90c0f78c85141c2256cb12b5a409132
Shubham Singh Thakur <stfushubham@gmail.com>
2022-08-23 23:13:11 +0530

    Time: 8 ms (84.35%), Space: 6 MB (66.08%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
+    int umap(char c){
+        switch(c){
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X' : return 10;
+            case 'L' : return 50;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            case 'M' : return 1000;
+            default : return -1;
+        }
+    }
+    
     int romanToInt(string s) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        unordered_map<char, int> umap = {
-            { 'I' , 1 },
-           { 'V' , 5 },
-           { 'X' , 10 },
-           { 'L' , 50 },
-           { 'C' , 100 },
-           { 'D' , 500 },
-           { 'M' , 1000 }
-        };
         
-        int res = umap[s.back()], n = s.size();
+        int res = umap(s.back()), n = s.size();
         
         for(int i=n-2; i>=0; i--){
-            if(umap[s[i]] < umap[s[i+1]]) res -= umap[s[i]];
-            else res += umap[s[i]];
+            if(umap(s[i]) < umap(s[i+1])) res -= umap(s[i]);
+            else res += umap(s[i]);
         }
         return res;
     }
