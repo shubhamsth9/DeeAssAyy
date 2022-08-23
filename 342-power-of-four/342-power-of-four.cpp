341e5670e9e539f223419e47174b857c00f8dd05
Shubham Singh Thakur <stfushubham@gmail.com>
2022-08-23 10:19:57 +0530

    Time: 3 ms (58.67%), Space: 5.9 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -7,7 +7,7 @@ public:
 //     }
     
     bool isPowerOfFour(int n) {
-        return (n > 0) && ((n & (n-1)) == 0) && ((n+1)%3 == 2);
+        return (n > 0) && ((n & (n-1)) == 0) && ((n-1)%3 == 0);
         // if(n == 1) return true;
         // if(n <= 0) return false;
         // long long x = 4;
