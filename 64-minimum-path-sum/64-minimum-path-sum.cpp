119331c79e2b0cb998306749d0e7ddaf0dd746fa
Shubham Singh Thakur <stfushubham@gmail.com>
2022-08-24 09:41:05 +0530

    Time: 15 ms (57.19%), Space: 10.2 MB (6.30%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,43 +1,23 @@
 class Solution {
 public:
-//     int f(int m, int n, vector<vector<int>>& grid, vector<vector<int>>& dp){
+    int fun(int i, int j, int m, int n, vector<vector<int>>& dp, vector<vector<int>>& grid){
         
-//         if(m == 0 && n == 0) return grid[0][0];
+        if(i == m-1 && j == n-1) return grid[i][j];
         
-//         if(dp[m][n] != -1) return dp[m][n];
+        if(dp[i][j] != -1) return dp[i][j];
         
-//         int up = INT_MAX, left = INT_MAX;
+        int down = 100000, right = 100000;
+        if(i+1 < m) down = fun(i+1, j, m, n, dp, grid);
+        if(j+1 < n) right = fun(i, j+1, m, n, dp, grid);
         
-//         if(m-1 >= 0) up = grid[m][n] + f(m-1, n, grid, dp);
-//         if(n-1 >= 0) left = grid[m][n] + f(m, n-1, grid, dp);
-        
-//         return dp[m][n] = min(up, left);
-//     }
+        return dp[i][j] = grid[i][j] + min(down, right);
+    }
     
     int minPathSum(vector<vector<int>>& grid) {
         
         int m=grid.size(), n=grid[0].size();
-        vector<int> prev(n, INT_MAX);
-        
-        for(int i=0; i<m; i++){
-            vector<int> curr(n, INT_MAX);
-            for(int j=0; j<n; j++){
-                if(i == 0 && j == 0) curr[j] = grid[i][j];
-                else {
-                    
-                    int up = INT_MAX, left = INT_MAX;
-                    
-                    if(i > 0) up = grid[i][j] + prev[j];
-                    
-                    if(j > 0) left = grid[i][j] + curr[j-1];
-                    
-                    curr[j] = min(up, left);
-                }
-            }
-            prev = curr;
-        }
-        
-        return prev[n-1];
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return fun(0, 0, m, n, dp, grid);
         
     }
 };
