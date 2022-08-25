ac42ca542bb24901b418ed2a161aabcb5314760d
Shubham Singh Thakur <stfushubham@gmail.com>
2022-08-25 21:15:32 +0530

    Time: 7 ms (98.98%), Space: 8.5 MB (97.37%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool canConstruct(string note, string mag) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
         int freq[26] = {0};
         for(int i=0; i<mag.size(); i++) freq[mag[i]-'a']++;
         for(int i=0; i<note.size(); i++){
