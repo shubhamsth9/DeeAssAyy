5903038cdb9e652a460995b9d7960fa9b878071f
Shubham Singh Thakur <stfushubham@gmail.com>
2022-08-27 00:53:55 +0530

    Time: 3 ms (73.16%), Space: 6.1 MB (55.65%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool reorderedPowerOf2(int n) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        
         string s = sorted(n);
         for(int i=0; i<32; i++){
             if(s == sorted(1<<i)) return true;
