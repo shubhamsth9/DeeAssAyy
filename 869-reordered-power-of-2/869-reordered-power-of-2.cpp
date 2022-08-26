3b4939872f08538ed9f44d6cc4d69def0ad6c997
Shubham Singh Thakur <stfushubham@gmail.com>
2022-08-27 01:01:49 +0530

    Time: 7 ms (42.94%), Space: 6.6 MB (29.94%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -7,12 +7,15 @@ public:
         string s = to_string(n);
         sort(s.begin(), s.end());
         
+        unordered_set<string> uset;
+        
         for(int i=0; i<32; i++){
             string tmp = to_string(1<<i);
             sort(tmp.begin(), tmp.end());
             
-            if(s == tmp) return true;
+            uset.insert(tmp);
         }
+        if(uset.find(s) != uset.end()) return true;
         return false;
     }
 };
