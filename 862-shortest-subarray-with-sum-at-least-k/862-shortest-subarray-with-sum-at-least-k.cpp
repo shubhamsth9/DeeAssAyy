dd94f74641a9b47d28190bdcc52907f546c36daf
Shubham Singh Thakur <stfushubham@gmail.com>
2022-09-03 10:36:23 +0530

    Time: 743 ms (5.77%), Space: 115.9 MB (16.05%) - LeetHub

---
+++ 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.cpp
@@ -0,0 +1,25 @@
+typedef long long ll;
+
+class Solution {
+public:
+    int shortestSubarray(vector<int>& nums, int k) {
+        ll res = 1e18, n = nums.size(), sum = 0;
+        priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> pq; 
+        
+        for(ll i=0; i<n; i++){
+            sum += nums[i];
+            if(sum >= k){
+                res = min(res, i+1);
+            }
+            while(pq.size() && sum-pq.top().first >= k){
+                res = min(res, i-pq.top().second);
+                pq.pop();
+            }
+            
+            pq.push({sum, i});
+            if(res == 1) return 1;
+        }
+        
+        return res == 1e18 ? -1 : res;
+    }
+};
