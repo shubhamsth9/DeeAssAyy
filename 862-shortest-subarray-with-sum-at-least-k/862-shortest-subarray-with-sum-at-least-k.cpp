f8095decca67ca2b109717ac6247fa528dee972c
Shubham Singh Thakur <stfushubham@gmail.com>
2022-09-03 10:37:06 +0530

    Time: 524 ms (20.03%), Space: 116.2 MB (12.87%) - LeetHub

--- 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.cpp
+++ 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.cpp
@@ -3,6 +3,9 @@ typedef long long ll;
 class Solution {
 public:
     int shortestSubarray(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        
         ll res = 1e18, n = nums.size(), sum = 0;
         priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> pq; 
         
