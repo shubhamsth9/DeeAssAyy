32ee0c0d2ffb6b54f3cea9b7c8235a38af7ab3d9
Shubham Singh Thakur <stfushubham@gmail.com>
2022-09-04 15:45:25 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (38.06%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    void dfs(int num, int n, int K, vector<int> &res) {
-      if (n == 0) 
-          res.push_back(num);
-      else {
-          int dig = num % 10;
-          if (dig + K <= 9)
-              dfs(num * 10 + dig + K, n - 1, K, res);
-          if (K != 0 && dig >= K)
-              dfs(num * 10 + dig - K, n - 1, K, res);
+    void util(int i, int n, int k, vector<int>&res){
+        if(n == 0){
+            res.push_back(i);
+            return;
         }
+        if(i%10 + k <= 9) util(i*10 + ((i%10) + k), n-1, k, res);
+        if(k != 0 && i%10 - k >= 0) util(i*10 + ((i%10) - k), n-1, k, res);
+        
     }
-    vector<int> numsSameConsecDiff(int N, int K) {
-         vector<int> res;
-        for (auto num = 1; num <= 9; ++num) 
-            dfs(num, N - 1, K, res);
+    vector<int> numsSameConsecDiff(int n, int k) {
+        vector<int> res;
+        for(int i=1; i<=9; i++){
+            util(i, n-1, k, res);
+        }
         return res;
     }
 };
