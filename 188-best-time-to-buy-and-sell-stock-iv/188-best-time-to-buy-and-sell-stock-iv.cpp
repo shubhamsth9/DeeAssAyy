c7a4f68c01a914d14564c9934948dde29505de8c
Shubham Singh Thakur <stfushubham@gmail.com>
2022-09-10 22:55:04 +0530

    Time: 1903 ms (5.01%), Space: 11.7 MB (60.37%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
-        int n = (int)prices.size(), ret = 0, v, p = 0;
-        priority_queue<int> profits;
-        stack<pair<int, int> > vp_pairs;
-        while (p < n) {
-            // find next valley/peak pair
-            for (v = p; v < n - 1 && prices[v] >= prices[v+1]; v++);
-            for (p = v + 1; p < n && prices[p] >= prices[p-1]; p++);
-            // save profit of 1 transaction at last v/p pair, if current v is lower than last v
-            while (!vp_pairs.empty() && prices[v] < prices[vp_pairs.top().first]) {
-                profits.push(prices[vp_pairs.top().second-1] - prices[vp_pairs.top().first]);
-                vp_pairs.pop();
+        if (k == 0)
+            return 0;
+        if (prices.size() == 0 || prices.size() == 1)
+            return 0;
+
+        vector<vector<int>> dp(k + 1, vector<int>(prices.size()));
+        for (int i = 0; i < dp.size(); i++)
+        {
+            for (int j = 0; j < dp[i].size(); j++)
+            {
+                if (i == 0)
+                    dp[i][j] = 0;
+                else if (j == 0)
+                    dp[i][j] = 0;
+                else
+                {
+                    int x;
+                    int mx = INT_MIN;
+                    for (int k = 0; k < j; k++)
+                    {
+                        x = prices[j] - prices[k] + dp[i - 1][k];
+                        mx = max(x, mx);
+                    }
+                    dp[i][j] = max(dp[i][j - 1], mx);
+                }
             }
-            // save profit difference between 1 transaction (last v and current p) and 2 transactions (last v/p + current v/p),
-            // if current v is higher than last v and current p is higher than last p
-            while (!vp_pairs.empty() && prices[p-1] >= prices[vp_pairs.top().second-1]) {
-                profits.push(prices[vp_pairs.top().second-1] - prices[v]);
-                v = vp_pairs.top().first;
-                vp_pairs.pop();
-            }
-            vp_pairs.push(pair<int, int>(v, p));
         }
-        // save profits of the rest v/p pairs
-        while (!vp_pairs.empty()) {
-            profits.push(prices[vp_pairs.top().second-1] - prices[vp_pairs.top().first]);
-            vp_pairs.pop();
-        }
-        // sum up first k highest profits
-        for (int i = 0; i < k && !profits.empty(); i++) {
-            ret += profits.top();
-            profits.pop();
-        }
-        return ret;
+        return dp[dp.size() - 1][prices.size() - 1];
     }
 };
