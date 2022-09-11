e402b4e2678d46060628feb6db42d78e60bf6768
Shubham Singh Thakur <stfushubham@gmail.com>
2022-09-11 10:07:44 +0530

    Time: 7 ms (100.00%), Space: 10.4 MB (20.00%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int partitionString(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
         int res = 1, n=s.size(), flag = 0;
         // unordered_set<char> uset;
         for(int i=0; i<n; i++){
