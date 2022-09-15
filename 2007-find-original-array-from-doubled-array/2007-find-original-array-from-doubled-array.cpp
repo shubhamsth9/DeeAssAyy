665edf44c18f30599dbe124aa2d1bee27d9bc141
Shubham Singh Thakur <stfushubham@gmail.com>
2022-09-15 22:23:22 +0530

    Time: 921 ms (20.14%), Space: 144.2 MB (51.60%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -4,26 +4,15 @@ public:
         int n = changed.size();
         if(n%2) return {};
         vector<int> res;
+        sort(begin(changed), end(changed));
         map<int, int> mp;
         for(auto it: changed) mp[it]++;
-        for(auto it:mp){
-            if(it.second == 0) continue;
-            if(mp.find(it.first*2) == mp.end() || mp[it.first] > mp[it.first*2]) return {};
-            else {
-                if(it.first == 0){
-                    if(it.second%2) return {};
-                    for(int i=0; i<(it.second/2); i++) res.push_back(0);
-                    
-                }
-                else{
-                    for(int i=0; i<it.second; i++){
-                        res.push_back(it.first);
-                        mp[it.first]--;
-                        mp[it.first*2]--;
-                    }
-                }
-                
-            }
+        for(int i=0; i<n; i++){
+            if(mp[changed[i]] == 0) continue;
+            if(mp[changed[i]*2] == 0) return {};
+            res.push_back(changed[i]);
+            mp[changed[i]]--;
+            mp[changed[i]*2]--;
         }
         return res;
     }
