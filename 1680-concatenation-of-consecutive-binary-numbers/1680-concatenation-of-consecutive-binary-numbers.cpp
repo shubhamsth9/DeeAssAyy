dc18c2eec5832b3eb6b9870a61a7e3d6f2e7e64f
Shubham Singh Thakur <stfushubham@gmail.com>
2022-09-23 19:16:30 +0530

    Time: 98 ms (63.59%), Space: 6 MB (22.83%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -4,7 +4,7 @@ public:
         return log2(n)+1;
     }
     int concatenatedBinary(int n) {
-        long long res = 0;
+        long res = 0;
         int m = 1e9+7;
         for(int i=1; i<=n; i++){
             int len = nofbits(i);
