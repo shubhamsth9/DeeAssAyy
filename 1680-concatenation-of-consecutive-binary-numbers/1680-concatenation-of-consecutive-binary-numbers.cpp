5a561a3a0508d69d231cb2b295b152f8114cc5e1
Shubham Singh Thakur <stfushubham@gmail.com>
2022-09-23 19:33:53 +0530

    Time: 28 ms (98.37%), Space: 6 MB (47.83%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int concatenatedBinary(int n) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        
         long res = 0, len = 0;
         int m = 1e9+7;
         for(int i=1; i<=n; i++){
