02afe58e7963e56f68731ce5de549b1a9d27a7bf
Shubham Singh Thakur <stfushubham@gmail.com>
2022-09-25 10:10:04 +0530

    Time: 330 ms (25.00%), Space: 88.7 MB (25.00%) - LeetHub

--- 2420-find-all-good-indices/2420-find-all-good-indices.cpp
+++ 2420-find-all-good-indices/2420-find-all-good-indices.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
     vector<int> goodIndices(vector<int>& nums, int k) {
-        int n=nums.size(), cnt = 1;
-        vector<bool> left(n, false), right(n, false);
-        
-        for(int i=1; i<n; i++){
-            if(nums[i] <= nums[i-1]){
-                cnt++;
-                if(cnt >= k) left[i] = true;
-            }
-            else cnt = 1;
-        }
-        cnt = 1;
-        for(int i=n-2; i>=0; i--){
-            if(nums[i] <= nums[i+1]){
-                cnt++;
-                if(cnt >= k) right[i] = true;
-            }
-            else cnt = 1;
-        }
+        int n=nums.size();
         vector<int> res;
+        vector<int> left(n+1, 1), right(n+1, 1);
         
-        if(k == 1){
-            for(int i=1; i<n-1; i++){
-                res.push_back(i);
-            }
-            return res;
-        }
+        for(int i=1; i<n; i++)
+            if(nums[i-1] >= nums[i])
+                left[i] = left[i-1]+1;
         
+        for(int i=n-2; i>=0; i--)
+            if(nums[i] <= nums[i+1])
+                right[i] = right[i+1]+1;
+                
         for(int i=1; i<n-1; i++){
-            if(left[i-1] && right[i+1]) res.push_back(i);
+            if(left[i-1] >= k && right[i+1] >= k) res.push_back(i);
         }
         
         return res;
