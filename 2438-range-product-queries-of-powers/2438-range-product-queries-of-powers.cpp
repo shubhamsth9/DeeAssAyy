4252ce3ec209f263b5f55ba80320b9bf8b88a5af
Shubham Singh Thakur <stfushubham@gmail.com>
2022-10-15 22:53:24 +0530

    Time: 831 ms (15.38%), Space: 143.3 MB (15.38%) - LeetHub

--- 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.cpp
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<int> productQueries(int n, vector<vector<int>>& q) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        
         int m = 1000000007;
         vector<int> powers, res;
         for(int i=0; i<32; i++){
@@ -10,9 +13,10 @@ public:
         for(int i=0; i<q.size(); i++){
             long temp = 1;
             for(int j = q[i][0]; j <= q[i][1]; j++){
-                temp = (temp * powers[j])%m;
+                temp *= powers[j];
+                temp %= m;
             }
-            res.push_back(temp);
+            res.push_back((int)temp);
         }
         return res;
     }
