787062451a43867354967005562ad5079c13beb5
Shubham Singh Thakur <stfushubham@gmail.com>
2022-10-17 22:20:17 +0530

    Time: 19 ms (6.98%), Space: 9 MB (68.88%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,40 +1,60 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        vector<char> s;
-        int cnt = 1;
-        int i;
-        for(i = 1; i<chars.size(); i++){
-            if(chars[i] == chars[i-1]){
+        
+        int n = chars.size(), i = 0, k = 0;
+        
+        while(i < n){
+            char ch = chars[i];
+            int cnt = 0;
+            while(i < n && chars[i] == ch){
                 cnt++;
+                i++;
             }
-            else{
-                s.push_back(chars[i-1]);
-                if(cnt > 1){
-                    if(cnt > 9){
-                        string tmp = to_string(cnt);
-                        for(auto ch:tmp){
-                            s.push_back(ch);
-                        }
-                    }
-                    else
-                        s.push_back(cnt + '0');
-                }
-                cnt = 1;
-            }
-        }
-        s.push_back(chars[i-1]);
-        if(cnt > 1){
-            if(cnt > 9){
+            chars[k++] = ch;
+            if(cnt > 1){
                 string tmp = to_string(cnt);
-                for(auto ch:tmp){
-                    s.push_back(ch);
+                for(auto c:tmp){
+                    chars[k++] = c;
                 }
             }
-            else
-                s.push_back(cnt + '0');
         }
-        chars = s;
-        return chars.size();
+        return k;
+        
+        // vector<char> s;
+        // int cnt = 1;
+        // int i;
+        // for(i = 1; i<chars.size(); i++){
+        //     if(chars[i] == chars[i-1]){
+        //         cnt++;
+        //     }
+        //     else{
+        //         s.push_back(chars[i-1]);
+        //         if(cnt > 1){
+        //             if(cnt > 9){
+        //                 string tmp = to_string(cnt);
+        //                 for(auto ch:tmp){
+        //                     s.push_back(ch);
+        //                 }
+        //             }
+        //             else
+        //                 s.push_back(cnt + '0');
+        //         }
+        //         cnt = 1;
+        //     }
+        // }
+        // s.push_back(chars[i-1]);
+        // if(cnt > 1){
+        //     if(cnt > 9){
+        //         string tmp = to_string(cnt);
+        //         for(auto ch:tmp){
+        //             s.push_back(ch);
+        //         }
+        //     }
+        //     else
+        //         s.push_back(cnt + '0');
+        // }
+        // chars = s;
+        // return chars.size();
     }
 };
