1398050c5ad86a01c19ff21fde68cec4cab02c12
Shubham Singh Thakur <stfushubham@gmail.com>
2022-10-19 12:35:47 +0530

    Time: 19 ms (72.29%), Space: 12.8 MB (39.56%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    bool static comp(pair<int, string> &a, pair<int, string> &b){
-        if(a.first == b.first){
-            return a.second < b.second;
-        }
-        return a.first > b.first;
-    }
+//     bool static comp(pair<int, string> &a, pair<int, string> &b){
+//         if(a.first == b.first){
+//             return a.second < b.second;
+//         }
+//         return a.first > b.first;
+//     }
     
     vector<string> topKFrequent(vector<string>& words, int k) {
         unordered_map<string, int> map;
@@ -14,7 +14,13 @@ public:
         }
         vector<pair<int, string>> vec;
         for(auto it:map) vec.push_back({it.second, it.first});
-        sort(vec.begin(), vec.end(), comp);
+        sort(vec.begin(), vec.end(), [](pair<int, string> &a, pair<int, string> &b) -> bool
+             {
+                 if(a.first == b.first){
+                    return a.second < b.second;
+                }
+                return a.first > b.first;
+             });
         vector<string> res;
         for(int i=0; i<k; i++) res.push_back(vec[i].second);
         return res;
