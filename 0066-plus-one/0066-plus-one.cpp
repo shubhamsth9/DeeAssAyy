3f56b99f1823fc7e744333c963b5ed00f35775a2
Shubham Singh Thakur <stfushubham@gmail.com>
2022-11-04 23:32:09 +0530

    Time: 3 ms (68.82%), Space: 8.9 MB (12.48%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
         int n = digits.size();
         for(int i = n-1; i>=0; i--){
             if(digits[i] == 9) {
