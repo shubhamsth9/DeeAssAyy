dd6da8942c02c01d5db82da4cdf3bea299ac6e15
Shubham Singh Thakur <stfushubham@gmail.com>
2022-11-09 08:32:39 +0530

    Time: 4 ms (34.39%), Space: 6.2 MB (95.85%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -6,9 +6,7 @@ public:
         for(auto ch:s){
             if(ch == '(' || ch == '{' || ch == '[') st.push(ch);
             else{
-                if(st.size() && (ch - st.top() == 1 || ch - st.top() == 2)){
-                    st.pop();
-                }
+                if(st.size() && (ch - st.top() == 1 || ch - st.top() == 2)) st.pop();
                 else return false;
             }
         }
